e3f4331e490945948d858540102ddc4092d81bfe
Samuel Lin <samuelddlin@gmail.com>
2021-02-17 21:58:46 -0800

    finished ex 3

---
+++ Chapter 2/ex3.cpp
@@ -0,0 +1,22 @@
+#include <iostream> //part a
+using namespace std; //part b
+
+int main() {
+
+    int num1, num2, num3, average; //part c
+
+    //part d
+    num1 = 125;
+    num2 = 28;
+    num3 = -25;
+
+    average = (num1 + num2 + num3)/3; //part e
+
+    //part f
+    cout << "num1: " << num1 << endl;
+    cout << "num2: " << num2 << endl;
+    cout << "num3: " << num3 << endl;
+    cout << "average: " << average << endl;
+
+    return 0;
+}
